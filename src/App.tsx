411c234c248f94e5889cd44ed5620b0975d115fe
sionyeom <duatldhs5555@gmail.com>
2022-11-29 13:06:29 +0900

    feat: 라우팅 URL 지정

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,24 @@
 import TodoApp from "./container/TodoApp";
 import GithubProfileLoader from "./container/GithubProfileLoader";
-import TutorialLoader from "./container/TutorialLoader";
+import Tutorial from "./Page/Tutorial/Tutorial";
+import RouterDefine from "./routing/router-define";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
+import Home from "./Page/Home/Home";
+import Login from "./Page/Login/Login";
+import { useEffect } from "react";
 const App = () => {
-  return <TutorialLoader />;
+  // auth
+
+  // Local Storage 테스트 코드
+  return (
+    <BrowserRouter>
+      <Routes>
+        <Route path={RouterDefine.HOME} element={<Home />} />
+        <Route path={RouterDefine.Login} element={<Login />} />
+        <Route path={RouterDefine.MAIN_PAGE} element={<Tutorial />} />
+      </Routes>
+    </BrowserRouter>
+  );
 };
 
 export default App;
---
+++ src/routing/router-define.tsx
@@ -0,0 +1,7 @@
+const RouterDefine = {
+  HOME: "/",
+  Login: "/login",
+  MAIN_PAGE: "/main",
+};
+
+export default RouterDefine;
