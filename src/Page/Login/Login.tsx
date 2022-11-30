5f9d48bb20b88a97314c52714fddba6aed78af13
sionyeom <duatldhs5555@gmail.com>
2022-11-30 17:56:06 +0900

    feat: 로그인 상태관리 적용

--- src/Page/Login/Login.tsx
+++ src/Page/Login/Login.tsx
@@ -1,22 +1,36 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
 import "./styles.module.scss";
 import Button from "@mui/material/Button";
-import axios from "axios";
 import LoginUtils from "./utils/LoginUtils";
+
+import { useNavigate } from "react-router-dom";
+
 type Props = {};
 
 const Login = (props: Props) => {
-  console.log("login");
-  const { handleLogin } = LoginUtils();
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    // 토큰 값 있을 시 로그인 접근 불가
+    if (localStorage.getItem("token")) {
+      navigate("/main");
+    }
+  });
+
+  const { handleLogin, handleData, data } = LoginUtils();
+
   return (
     <div>
       <Button
         variant="text"
-        onClick={() => {
+        onClick={(e) => {
           handleLogin();
         }}
       >
         로그인
+      </Button>{" "}
+      <Button variant="text" onClick={handleData}>
+        2 로그인
       </Button>
     </div>
   );
--- src/Page/Login/utils/LoginUtils.ts
+++ src/Page/Login/utils/LoginUtils.ts
@@ -1,10 +1,35 @@
 import ApiUtils from "@/utils/api/axiosHelper";
+
+import { useDispatch, useSelector } from "react-redux";
+import { RootState } from "@/modules";
+import { postLoginAllThunk } from "@/modules/Login";
+import { LoginDescription } from "@/types/common";
 interface Utils {
   handleLogin: () => void;
+  handleData: () => void;
+  data: any;
 }
 
 export default function LoginUtils(): Utils {
-  const handleLogin = () => {};
+  const { data, loading, error } = useSelector(
+    (state: RootState) => state.Login.Login
+  );
 
-  return { handleLogin };
+  const dispatch = useDispatch();
+
+  const handleLogin = async () => {
+    try {
+      dispatch(
+        postLoginAllThunk("yeom", "yeom").then((e: any) => {
+          console.log(e);
+        })
+      );
+    } catch (e) {}
+  };
+
+  const handleData = () => {
+    console.log(JSON.parse(localStorage.getItem(LoginDescription.TOKEN_KEY)));
+  };
+
+  return { handleLogin, handleData, data };
 }
