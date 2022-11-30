5b19a4e0c91581acc910a92ad528fcb9105a4fa9
sionyeom <duatldhs5555@gmail.com>
2022-11-30 17:54:50 +0900

    feat: isAuth 함수 작성

--- src/utils/common/isAuth.ts
+++ src/utils/common/isAuth.ts
@@ -1,1 +1,10 @@
-const isAuth = () => localStorage.getItem("token");
+const isAuth = () => {
+  if (localStorage.getItem("token")) {
+    const token = JSON.parse(localStorage.getItem("token"));
+    return token;
+  } else {
+    console.log("no token");
+  }
+};
+
+export default isAuth;
