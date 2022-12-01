c1c0eea1f95cd1846a0e47e7e2ad198fa79296c9
sionyeom <duatldhs5555@gmail.com>
2022-12-01 18:16:56 +0900

    feat: routing-define 추가

--- src/routing/router-define.tsx
+++ src/routing/router-define.tsx
@@ -1,7 +1,11 @@
 const RouterDefine = {
+  // 페이지
   HOME: "/",
   Login: "/login",
   MAIN_PAGE: "/main",
+  // ERROR HANDLER
+  ERROR_EXPIRED_ACCESS_TOKEN: "/auth/access",
+  ERROR_EXPIRED_REFRESH_TOKEN: "/auth/refresh",
 };
 
 export default RouterDefine;
