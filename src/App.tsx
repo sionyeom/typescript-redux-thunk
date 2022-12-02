98d4ec359fd09250e1cc22757e29c28c134f77bf
sionyeom <duatldhs5555@gmail.com>
2022-12-02 16:40:29 +0900

    feat: PrivateRoute 적용

--- src/App.tsx
+++ src/App.tsx
@@ -5,7 +5,8 @@ import RouterDefine from "./routing/router-define";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Home from "./Page/Home/Home";
 import Login from "./Page/Login/Login";
-import { useEffect } from "react";
+import PrivateRoute from "./routing/components/PrivateRoute";
+import NotFound from "./Page/NotFound/NotFound";
 const App = () => {
   // auth
 
@@ -13,9 +14,22 @@ const App = () => {
   return (
     <BrowserRouter>
       <Routes>
-        <Route path={RouterDefine.HOME} element={<Home />} />
-        <Route path={RouterDefine.Login} element={<Login />} />
-        <Route path={RouterDefine.MAIN_PAGE} element={<Tutorial />} />
+        {/* 인증 여부 상관 없이 접근 가능한 페이지 */}
+        <Route path={RouterDefine.LANDING_PAGE} element={<Home />} />
+
+        {/* 인증을 하지 않아야 접근 가능한 페이지 */}
+        <Route element={<PrivateRoute authentication={false} />}>
+          <Route path={RouterDefine.LOGIN} element={<Login />} />
+        </Route>
+
+        {/* 인증을 해야만 접근 가능한 페이지 */}
+        <Route element={<PrivateRoute authentication={true} />}>
+          <Route path={RouterDefine.MAIN_PAGE} element={<Tutorial />} />
+          {/* 권한 체크 필요한 페이지 정의(관리자, 파트너) */}
+        </Route>
+
+        {/* 인증/권한 여부 상관 없이 접근 가능한 ERROR 페이지 정의 */}
+        <Route path="/*" element={<NotFound />} />
       </Routes>
     </BrowserRouter>
   );
