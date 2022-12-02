fe6837a045edc92f45d16ce708838aff545cd688
sionyeom <duatldhs5555@gmail.com>
2022-12-02 16:39:03 +0900

    fix: HOME 컴포넌트 수정

--- src/Page/Home/Home.tsx
+++ src/Page/Home/Home.tsx
@@ -1,13 +1,21 @@
 import React from "react";
 import "./styles.module.scss";
 import Button from "@mui/material/Button";
-import axios from "axios";
+import { useNavigate } from "react-router-dom";
+
 type Props = {};
 
 const Home = (props: Props) => {
+  const navigate = useNavigate();
   return (
     <div>
-      <Button variant="text" onClick={() => {}}>
+      <div> 랜딩 페이지</div>
+      <Button
+        variant="text"
+        onClick={() => {
+          navigate("/login");
+        }}
+      >
         로그인
       </Button>
     </div>
