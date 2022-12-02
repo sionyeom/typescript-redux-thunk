dc94a4ba9013c36b8c46be139c101f6fc955f05d
sionyeom <duatldhs5555@gmail.com>
2022-12-02 17:22:33 +0900

    feat: 권한 체크 라우팅

--- src/App.tsx
+++ src/App.tsx
@@ -3,9 +3,16 @@ import GithubProfileLoader from "./container/GithubProfileLoader";
 import Tutorial from "./Page/Tutorial/Tutorial";
 import RouterDefine from "./routing/router-define";
 import { BrowserRouter, Route, Routes } from "react-router-dom";
+// 로그인 및 권한 여부 체크 컴포넌트
+import PrivateRoute from "./routing/components/PrivateRoute";
+import ProtectedRoute from "./routing/components/ProtectedRoute";
+import ProtectedForAdminRoute from "./routing/components/ProtectedForAdminRoute";
+import ProtectedForPartnerRoute from "./routing/components/ProtectedForPartnerRoute";
+// 페이지 컴포넌트
 import Home from "./Page/Home/Home";
 import Login from "./Page/Login/Login";
-import PrivateRoute from "./routing/components/PrivateRoute";
+import Admin from "./Page/Admin/Admin";
+import Partner from "./Page/Partner/Partner";
 import NotFound from "./Page/NotFound/NotFound";
 const App = () => {
   // auth
@@ -25,7 +32,16 @@ const App = () => {
         {/* 인증을 해야만 접근 가능한 페이지 */}
         <Route element={<PrivateRoute authentication={true} />}>
           <Route path={RouterDefine.MAIN_PAGE} element={<Tutorial />} />
-          {/* 권한 체크 필요한 페이지 정의(관리자, 파트너) */}
+
+          {/* 관리자 권한 체크 필요한 페이지 정의 */}
+          <Route element={<ProtectedForAdminRoute />}>
+            <Route path={RouterDefine.ADMIN_MAIN} element={<Admin />} />
+          </Route>
+
+          {/* 파트너 권한 체크 필요한 페이지 정의 */}
+          <Route element={<ProtectedForAdminRoute />}>
+            <Route path={RouterDefine.PARTNER_MAIN} element={<Partner />} />
+          </Route>
         </Route>
 
         {/* 인증/권한 여부 상관 없이 접근 가능한 ERROR 페이지 정의 */}
